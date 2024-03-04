60f0dcebd47f484b97de62a1f96d53bb6d10c454
asnts <annandasants@gmail.com>
2024-03-03 21:19:48 -0300

    git commit -m "feat: Create package routes and class MovieOmdb.java"

---
+++ webmovies/src/main/java/br/com/project/webmovies/routes/MovieOmdb.java
@@ -0,0 +1,31 @@
+package br.com.project.webmovies.routes;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JacksonStdImpl;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@JacksonStdImpl
+@SpringBootApplication
+public class MovieOmdb {
+
+	@JsonProperty("Title")
+	private String title;
+	@JsonProperty("Year")
+	private  String year;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+}
