b9fbda49ca6dac985d2811cd3ea2351b1e74264b
asnts <annandasants@gmail.com>
2024-03-16 18:32:05 -0300

    git commit -m "feat: update WebMoviesApplication.java"

--- webmovies/src/main/java/br/com/project/webmovies/WebMoviesApplication.java
+++ webmovies/src/main/java/br/com/project/webmovies/WebMoviesApplication.java
@@ -1,5 +1,6 @@
 package br.com.project.webmovies;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
